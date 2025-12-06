24764fef3090ac34138745ca85398ab0a4ea08ba
Saulo Román Santillán Nava <romansantillan1998@outlook.com>
2025-12-05 19:37:34 -0600

    Url backend changed

--- src/api/abrasa.api.ts
+++ src/api/abrasa.api.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const { VITE_BACKEND_URL } = import.meta.env
-
 export const abrasaAPI = axios.create({
     baseURL: "https://abrasa-tickets-backend-production.up.railway.app"
 })
